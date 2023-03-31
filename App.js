f431d42ebe0c4a5a788f3c55dd6f9fe5ac14ce99
hwangs12 <sungjun94@gmail.com>
2023-03-31 16:14:05 -0400

    worked to improve speed

--- App.js
+++ App.js
@@ -1,41 +1,52 @@
-
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image, TouchableWithoutFeedback, TouchableOpacity,
-TouchableHighlight, Alert, Dimensions, Button } from 'react-native';
-import {useState} from 'react'
+import { StatusBar } from "expo-status-bar";
+import {
+    StyleSheet,
+    Text,
+    View,
+    SafeAreaView,
+    Image,
+    TouchableWithoutFeedback,
+    TouchableOpacity,
+    TouchableHighlight,
+    Alert,
+    Dimensions,
+    Button,
+} from "react-native";
+import { useState } from "react";
 
 export default function App() {
+    [color, setColor] = useState("yellow")[(speed, setSpeed)] = useState(5);
 
-  [color, setColor] = useState('yellow')
+    const changeColor = (backgroundColor) => {
+        if (backgroundColor === "yellow") {
+            setColor("dodgerblue");
+        } else {
+            setColor("yellow");
+        }
+    };
 
-  const changeColor = (backgroundColor) => {
-    if (backgroundColor === 'yellow') {
-      setColor('dodgerblue')
-    } else {
-      setColor('yellow')
-    }
-  }
-
-  return (
-    <SafeAreaView style={styles(color).container}>
-      <Text>HIHIHHI</Text>
-      <Button 
-        onPress={() => changeColor(color)}
-        title={'Click Me'}
-        style={{
-        backgroundColor: 'dodgerblue',
-        width: '50%',
-        height: 70
-      }} />
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={styles(color).container}>
+            <Text>HIHIHHI</Text>
+            <Button
+                onPress={() => changeColor(color)}
+                title={"Click Me"}
+                style={{
+                    backgroundColor: "dodgerblue",
+                    width: "50%",
+                    height: 70,
+                }}
+            />
+        </SafeAreaView>
+    );
 }
 
-const styles = (color) => StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: color,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = (color) =>
+    StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: color,
+            alignItems: "center",
+            justifyContent: "center",
+        },
+    });
